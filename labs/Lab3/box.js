d4182154fa8bcb8d345a22165f315a2abecc712a
Turtile <evanturtile.chen@mail.utoronto.ca>
2018-02-11 21:47:44 -0500

    Adding Week 3 and 4 stuff

---
+++ labs/Lab3/box.js
@@ -0,0 +1,23 @@
+var box = function() {
+    var boxobj = {};
+
+    // TODO
+    /* Go nuts in this program. A box has to have
+     * something the moment you open it up, right? :)
+     * Surprise me! (You MUST have at least a couple of
+     * methods inside this object that can run once
+     * called!!)
+     *
+     * Some suggestions can include the following:
+     *    - A box can have the length, width, and height
+     *    - A box can contain some delish candy!
+     *    - A box can be a house (can this even be practical?)
+     *    - ...and much more!
+     * This exercise is to help you understand how
+     * Object Oriented Programming (OOP) works. */
+
+
+
+    // Do NOT touch this line.
+    return boxobj;
+};
